f85d3c68ea5d3167014e3a92ec0a01c8af51aa68
tritan89 <61888066+tritan89@users.noreply.github.com>
2022-11-20 18:52:39 -0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/a3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/a3.dir/main.c.o"
+  "a3"
+  "a3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/a3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
